0c651bc10fbd1bf6c87c630a991bd943d520747e
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-01-03 11:55:43 +0700

    Added the SSL certificate

--- projects/core/src/index.ts
+++ projects/core/src/index.ts
@@ -3,6 +3,25 @@ import * as k8s from "@pulumi/kubernetes";
 import './dns'
 import './service.elk'
 import provider from "./provider";
+import * as fs from "node:fs";
+import * as path from "node:path";
+
+const certPath = path.resolve(__dirname, "../../../etc/archive/internal.mrida.ng/fullchain1.pem");
+const keyPath = path.resolve(__dirname, "../../../etc/archive/internal.mrida.ng/privkey1.pem");
+
+console.log(certPath)
+
+const tlsSecret = new k8s.core.v1.Secret("internal-mrida-tls", {
+	metadata: {
+		name: "internal-mrida-tls",
+		namespace: "default",
+	},
+	type: "kubernetes.io/tls",
+	data: {
+		"tls.crt": Buffer.from(fs.readFileSync(certPath)).toString("base64"),
+		"tls.key": Buffer.from(fs.readFileSync(keyPath)).toString("base64"),
+	},
+}, { provider });
 
 const traefik = new k8s.helm.v3.Chart("traefik", {
 	chart: "traefik",
