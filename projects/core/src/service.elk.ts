1494e94e2730e6d3e756f666058a0382b3d459f0
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-01-06 14:10:08 +0700

    Added the ELK stack

--- projects/core/src/service.elk.ts
+++ projects/core/src/service.elk.ts
@@ -1,44 +1,122 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as k8s from "@pulumi/kubernetes";
+import {tlsSecret} from "./tls";
 import provider from "./provider";
+import {traefik} from "./service.traefik";
 
-const elasticsearch = new k8s.helm.v3.Chart("elasticsearch", {
-	chart: "elasticsearch",
-	fetchOpts: {
+const eckOperatorNamespace = new k8s.core.v1.Namespace("eck-operator", {
+	metadata: { name: "elastic-system" },
+});
+
+const eckOperator = new k8s.helm.v3.Release("eck-operator", {
+	namespace: eckOperatorNamespace.metadata.name,
+	chart: "eck-operator",
+	version: "2.16.0",
+	repositoryOpts: {
 		repo: "https://helm.elastic.co",
 	},
-	values: {
-		//protocol: 'http',
-		createCert: true,
-		replicas: 1,
-		esJavaOpts: "-Xmx1g -Xms1g",
-		service: {
-			type: "NodePort",
-			port: 9200,
+});
+
+const elasticsearchCluster = new k8s.apiextensions.CustomResource("elasticsearch-cluster", {
+	apiVersion: "elasticsearch.k8s.elastic.co/v1",
+	kind: "Elasticsearch",
+	metadata: { name: "my-cluster" },
+	spec: {
+		version: "8.5.0",
+		http: {
+			tls: {
+				selfSignedCertificate: {
+					disabled: true,
+				},
+			},
+		},
+		nodeSets: [
+			{
+				name: "default",
+				count: 1,
+				config: {
+					"node.store.allow_mmap": "false",
+				},
+				volumeClaimTemplates: [
+					{
+						metadata: { name: "elasticsearch-data" },
+						spec: {
+							accessModes: ["ReadWriteOnce"],
+							storageClassName: "hostpath",
+							resources: { requests: { storage: "10Gi" } },
+						},
+					},
+				],
+			},
+		],
+	},
+});
+
+
+const kibana = new k8s.apiextensions.CustomResource("kibana-instance", {
+	apiVersion: "kibana.k8s.elastic.co/v1",
+	kind: "Kibana",
+	metadata: { name: "my-kibana" },
+	spec: {
+		version: "8.5.0",
+		http: {
+			tls: {
+				selfSignedCertificate: {
+					disabled: true,
+				},
+			},
+		},
+		count: 1,
+		elasticsearchRef: { name: elasticsearchCluster.metadata.name },
+	},
+});
+
+new k8s.apiextensions.CustomResource("kibana-ingressroute", {
+	apiVersion: "traefik.io/v1alpha1",
+	kind: "IngressRoute",
+	metadata: {
+		name: "kibana-ingressroute",
+		namespace: "default",
+	},
+	spec: {
+		entryPoints: ["websecure"],
+		routes: [
+			{
+				match: "Host(`kibana.internal.mrida.ng`)",
+				kind: "Rule",
+				services: [
+					{
+						name: "my-kibana-kb-http",
+						port: 5601,
+					},
+				],
+			},
+		],
+		tls: {
+			secretName: tlsSecret.metadata.name
 		},
 	},
-}, {provider});
+}, {provider, dependsOn: [traefik]});
 
-const elasticsearchHost = pulumi.interpolate`https://${elasticsearch.getResource("v1/Service", "elasticsearch-master").metadata.name}:9200`;
-
-// const kibana = new k8s.helm.v3.Chart("kibana", {
-// 	chart: "kibana",
-// 	version: "8.5.1",
-// 	fetchOpts: {
-// 		repo: "https://helm.elastic.co",
-// 	},
-// 	values: {
-// 		elasticsearchHosts: elasticsearchHost,
-// 		replicas: 1,
-// 		service: {
-// 			type: "NodePort",
-// 			port: 5601,
-// 		},
-// 	},
-// }, { provider });
-
-const elasticsearchService = elasticsearch.getResource("v1/Service", "elasticsearch-master");
-//const kibanaService = kibana.getResource("v1/Service", "kibana");
-
-export const elasticsearchUrl = elasticsearchService.status.loadBalancer.ingress[0].hostname;
-//export const kibanaUrl = kibanaService.status.loadBalancer.ingress[0].hostname;
+const apmServer = new k8s.apiextensions.CustomResource("apm-server-instance", {
+	apiVersion: "apm.k8s.elastic.co/v1",
+	kind: "ApmServer",
+	metadata: {
+		name: "my-apm-server",
+	},
+	spec: {
+		version: "8.5.0",
+		http: {
+			tls: {
+				selfSignedCertificate: {
+					disabled: true,
+				},
+			},
+		},
+		count: 1,
+		elasticsearchRef: { name: elasticsearchCluster.metadata.name },
+		config: {
+			"apm-server.rum.enabled": "true",
+			"apm-server.rum.allowed_origins": ["*"],
+		},
+	},
+}, { provider });
