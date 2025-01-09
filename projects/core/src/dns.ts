7a1cb01f911f3c40f730e71464fcc462840ac2c8
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-01-09 11:13:58 +0700

    Configured the DNS configuration to not use the host resolver but instead use the Cloudflare resolver.

--- projects/core/src/dns.ts
+++ projects/core/src/dns.ts
@@ -1,6 +1,20 @@
 import * as k8s from '@pulumi/kubernetes';
 import provider from './provider';
 
+/**
+ * CoreDNS is some scenarios is managed by the K8 environment and this means
+ * that we need to edit the configuration that CoreDNS uses.
+ *
+ * After making any changes, you may need to restart CoreDNS using the following
+ * command: .
+ *
+ * `kubectl rollout restart deployment coredns --namespace=kube-system`
+ *
+ *
+ * To view logs about CoreDNS errors, use the following command:
+ *
+ * `kubectl logs --namespace=kube-system --selector=k8s-app=kube-dns --follow`
+ */
 new k8s.core.v1.ConfigMap(
   'coredns',
   {
@@ -26,9 +40,10 @@ new k8s.core.v1.ConfigMap(
                 }
                 rewrite name regex  /(.*)-(.*)\\.internal\\.mrida\\.ng/ {1}.{2}.svc.cluster.local
                 prometheus :9153
-                forward . /etc/resolv.conf {
-                    max_concurrent 1000
-                }
+                forward . tls://1.1.1.1 tls://1.0.0.1 {
+                	tls_servername cloudflare-dns.com
+       				health_check 5s
+    			}
                 cache 30
                 loop
                 reload
