f38ca70184f77625b1ee7d63182adf66fd258de5
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-01-28 12:22:45 +0700

    Improved the code to ensure that packetbeat doesn't retain more than 2 days worth or 5GB of data.

--- projects/core/src/elastic/scripts/index.ts
+++ projects/core/src/elastic/scripts/index.ts
@@ -76,6 +76,47 @@ fetch(
 
 // noinspection HttpUrlsUsage
 fetch(
+  `http://${process.env['ELASTICSEARCH_HOSTNAME']}:9200/_ilm/policy/packetbeat`,
+  {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Basic ${btoa(`${process.env['ELASTICSEARCH_USERNAME']}:${process.env['ELASTICSEARCH_PASSWORD']}`)}`,
+    },
+    body: JSON.stringify({
+      policy: {
+        phases: {
+          hot: {
+            actions: {
+              rollover: {
+                max_age: '2d', // Set max_age to 2 days
+                max_primary_shard_size: '5gb', // Set max shard size to 5GB
+              },
+            },
+          },
+        },
+      },
+    }),
+  },
+)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(
+        `HTTP error! Status: ${response.status} ${response.statusText}`,
+      );
+    }
+    return response.json();
+  })
+  .then((data) => {
+    if (data.acknowledged !== true) {
+      throw new Error(`Unexpected response: ${JSON.stringify(data)}`);
+    }
+    console.log('Index template updated successfully:', data);
+  })
+  .catch((error) => console.error(error));
+
+// noinspection HttpUrlsUsage
+fetch(
   `http://${process.env['ELASTICSEARCH_HOSTNAME']}:9200/filebeat-${process.env['ELASTICSEARCH_VERSION']}/_rollover`,
   {
     method: 'POST',
