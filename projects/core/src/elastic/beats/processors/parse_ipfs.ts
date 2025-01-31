e2a31452f5b1d63efb2e3611785f17db3f9dbbbb
Mridang Agarwalla <mridang.agarwalla@gmail.com>
2025-01-31 16:40:09 +0700

    Removed the API secret as it was not used and added the logging configuration for the IPFS daemon

---
+++ projects/core/src/elastic/beats/processors/parse_ipfs.ts
@@ -0,0 +1,27 @@
+// @ts-ignore since this is not actually declared multiple times
+const ecsLevels: Record<string, string> = {
+  debug: 'debug',
+  info: 'info',
+  warn: 'warn',
+  error: 'error',
+  dpanic: 'critical',
+  fatal: 'critical',
+};
+
+// @ts-ignore since this is not actually declared multiple times
+const logPattern =
+  /^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z)\s+(debug|info|warn|error|dpanic|fatal)\s+(.*)$/;
+
+// @ts-ignore the unused warning since this method is actually used
+function process(event: Event): void {
+  const logMatch = event.Get<string>('message')?.match(logPattern);
+  if (logMatch) {
+    const logLevel = logMatch[2];
+    if (ecsLevels[logLevel]) {
+      event.Put('log.level', ecsLevels[logLevel]);
+    } else {
+      event.Put('log.level', 'unknown');
+    }
+    event.Put('message', logMatch[3]);
+  }
+}
